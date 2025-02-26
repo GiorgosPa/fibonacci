241f5035060ea20a64f6953b956f385f9f5af394
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2025-02-26 22:02:40 +0100

    Improve fibonacci interface

--- src/app/api.py
+++ src/app/api.py
@@ -3,20 +3,19 @@ import sys
 
 from fastapi import FastAPI
 
-from app.fibonacci import router as fibonacci_router
-from app.fibonacci import model as fibonacci_model
+from app import fibonacci
 
 
 def create() -> FastAPI:
     @contextlib.asynccontextmanager
     async def lifespan(app: FastAPI):
         sys.set_int_max_str_digits(50_000)
-        fibonacci_model.compute_fibonacci_number()
+        fibonacci.compute_fibonacci_number()
         yield
 
     app = FastAPI(title="Fibonacci API.",
                   lifespan=lifespan)
 
-    app.include_router(fibonacci_router.router)
+    app.include_router(fibonacci.router)
 
     return app
---
+++ src/app/fibonacci/__init__.py
@@ -0,0 +1,5 @@
+from app.fibonacci.model import compute_fibonacci_number
+from app.fibonacci.router import router
+
+
+__all__ = ["compute_fibonacci_number", "router"]
